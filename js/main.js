1000b8896e39978effde1f4024237b3857d863bc
AkHake <akanksha.hake123@gmail.com>
2026-01-04 22:28:01 +0530

    Fixed alignment with dark toggle

--- js/main.js
+++ js/main.js
@@ -167,3 +167,28 @@ function setActiveLink() {
 }
 
 window.addEventListener("scroll", setActiveLink);
+
+// const themeToggle = document.getElementById("themeToggle");
+
+// // Load saved theme
+// if (localStorage.getItem("theme") === "dark") {
+//   document.body.classList.add("dark");
+//   themeToggle.textContent = "☀️";
+// }
+
+// // Toggle theme
+// themeToggle.addEventListener("click", () => {
+//   document.body.classList.toggle("dark");
+
+//   const isDark = document.body.classList.contains("dark");
+//   themeToggle.textContent = isDark ? "☀️" : "🌙";
+//   localStorage.setItem("theme", isDark ? "dark" : "light");
+// });
+
+const toggle = document.getElementById("themeToggle");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  toggle.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
+
